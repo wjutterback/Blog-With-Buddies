047c9981372ff0710e888de922b362d0634b37dd
Will Utterback <uwill08@gmail.com>
2021-03-12 16:40:49 -0600

    Set up JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
-
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
